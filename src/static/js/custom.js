9d3ffa16bc9b8c8d4fca5aec2625c9f7b9e080e2
nvk1196 <48897412+nvk1196@users.noreply.github.com>
2019-08-22 14:02:52 -0500

    added error handling empty field

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,21 +1,28 @@
 //prevent resubmit on refresh
 if ( window.history.replaceState ) 
 {
-    window.history.replaceState( null, null, window.location.href );
+	window.history.replaceState( null, null, window.location.href );
 }
 	//---Main. Home----
-var need_package = new Boolean(false);
-var need_fragile = new Boolean(false);
-var est_item_size;
-var counter = 0; 
+	var need_package = new Boolean(false);
+	var need_fragile = new Boolean(false);
+	var est_item_size;
+	var counter = 0; 
 
-function nextForm(current_form) {
-	cal_total_cost();
+	function nextForm(current_form) {
+		cal_total_cost();
 
 	if (current_form == "PickUpForm")
 	{
-		document.getElementById("pickup_form").style.display = "none";
-		document.getElementById("question_form").style.display = "block";
+		if (document.getElementById("pick_up_full_name").value == "" ||
+			document.getElementById("pick_up_address").value == "" ||
+			document.getElementById("pick_up_state").value == "" ||
+			document.getElementById("pick_up_city").value == "" ||
+			document.getElementById("pick_up_zip").value == "")
+			{document.getElementById("empty_field_pick_up").style.display = "block"; return false;}
+
+	document.getElementById("pickup_form").style.display = "none";
+	document.getElementById("question_form").style.display = "block";
 	}
 //Return
 	else if (current_form == "QuestionForm_yes")	//it is a return
@@ -31,6 +38,9 @@ function nextForm(current_form) {
 	}
 	else if (current_form == "ReturnAmazonForm")		//upload Amazon QR code
 	{
+		if (document.getElementById("amazon_QR").value == "")
+			{document.getElementById("empty_field_amazon_qr").style.display = "block"; return false;}
+
 		document.getElementById("amazon_upload_form").style.display = "none";
 		cal_total_cost();
 		document.getElementById("total_cost_form").style.display = "block";
@@ -44,20 +54,17 @@ function nextForm(current_form) {
 	}		
 	else if (current_form == "UploadLabelForm")			//not Amazon return	
 	{
+		if (document.getElementById("return_label_1").value == "")
+			{document.getElementById("empty_field_upload_label").style.display = "block"; return false;}
 		document.getElementById("upload_label_form").style.display = "none";
 		document.getElementById("repackage_form").style.display = "block";		
 	}
 	else if (current_form == "Repackage_yes")		//if need repackage
-	{	need_package = true;
-		document.getElementById("repackage_form").style.display = "none";
-		document.getElementById("is_small_form").style.display = "block";
-				
-	}
-	else if (current_form == "UploadLabelForm")		//if need repackage
-	{
-		document.getElementById("upload_label_form").style.display = "none";
-		document.getElementById("is_small_form").style.display = "block";		
-	}
+		{	need_package = true;
+			document.getElementById("repackage_form").style.display = "none";
+			document.getElementById("is_small_form").style.display = "block";
+
+		}
 	else if (current_form == "Repackage_no")		//don't need repackage
 	{
 		document.getElementById("repackage_form").style.display = "none";
@@ -79,18 +86,18 @@ function nextForm(current_form) {
 	{
 		//est_item_size = document.getElementByName("item_size");
 		
-    	var elements = document.getElementsByName("item_size");
-    	for (var i = 0, l = elements.length; i < l; i++)
-    	{
+		var elements = document.getElementsByName("item_size");
+		for (var i = 0, l = elements.length; i < l; i++)
+		{
     		//alert("not in");
-       	 	if (elements[i].checked)
-       	 	{
-            est_item_size = elements[i].value;
-       	 	}
-   	 	}
+    		if (elements[i].checked)
+    		{
+    			est_item_size = elements[i].value;
+    		}
+    	}
    	 	//alert(est_item_size);
 		document.getElementById("est_item_size").value = est_item_size;		//set input value for est_item_size
-//alert("i'm in: " + est_item_size);
+		//alert("i'm in: " + est_item_size);
 		document.getElementById("choose_size_form").style.display = "none";
 		document.getElementById("fragile_form").style.display = "block";		
 
@@ -111,7 +118,7 @@ function nextForm(current_form) {
 		document.getElementById("total_cost_form").style.display = "block";
 
 	}
-			
+
 	//COMMON
 	else if (current_form == "TotalCostForm")
 	{
@@ -120,11 +127,15 @@ function nextForm(current_form) {
 	}
 	else if (current_form == "EnterPhoneForm")
 	{
+		if (document.getElementById("phone_number").value == "")
+			{document.getElementById("empty_field_enter_phone").style.display = "block"; return false;}
 		document.getElementById("enter_phone_form").style.display = "none";
 		document.getElementById("verify_phone_form").style.display = "block";
 	}	
-		else if (current_form == "SubmitForm")
+	else if (current_form == "SubmitForm")
 	{	
+		if (document.getElementById("verify_code").value == "")
+			{document.getElementById("empty_field_enter_code").style.display = "block"; return false;}
 		document.getElementById("verify_phone_form").style.display = "none";
 		document.getElementById("final_form").style.display = "block";
 
@@ -135,35 +146,36 @@ function nextForm(current_form) {
 		// window.location.href = "/user_message.html";
 
 
-
-
-
 	}
 	
 	//Regular Ship
-		else if (current_form == "QuestionForm_no")
+	else if (current_form == "QuestionForm_no")
 	{
 		document.getElementById("question_form").style.display = "none";
 		document.getElementById("shipto_form").style.display = "block";		
 	}
-		else if (current_form == "ShipToForm")
-	{	need_package = true;
-		document.getElementById("shipto_form").style.display = "none";
-		document.getElementById("is_small_form").style.display = "block";
-		
-	}
+	else if (current_form == "ShipToForm")
+		{	
+			if (document.getElementById("ship_to_full_name").value == "" ||
+			document.getElementById("ship_to_address").value == "" ||
+			document.getElementById("ship_to_state").value == "" ||
+			document.getElementById("ship_to_city").value == "" ||
+			document.getElementById("ship_to_zip").value == "")
+			{document.getElementById("empty_field_ship_to").style.display = "block"; return false;}
 
-			//counter++;alert("Counter: " + counter);
+			need_package = true;
+			document.getElementById("shipto_form").style.display = "none";
+			document.getElementById("is_small_form").style.display = "block";
 
+		}
 
-
-}
+		}
 	//---Calculate total cost
 	function cal_total_cost(){
-	var total_cost = 0.0; 
-	var pick_up_fee = 4; 
-	var package_fee = 2; 
-	var fragile_fee = 2; 
+		var total_cost = 0.0; 
+		var pick_up_fee = 4; 
+		var package_fee = 2; 
+		var fragile_fee = 2; 
 
 	//pickup fee
 	total_cost += pick_up_fee;
@@ -191,21 +203,21 @@ function nextForm(current_form) {
 	document.getElementById("total_cost").innerHTML = "Total: " + total_cost + " $";
 
 	document.getElementById("cost").value = total_cost;		//set input value for cost for django to pull	
-	}
+}
 
 //Send 4 digit code for phone verify without refresh. Guide: https://www.youtube.com/watch?v=KgnPSmrQrXI
 var temp_phone
 $(document).on('submit', '#VerifyPhoneNumber', function(e){
 	e.preventDefault();
 	$.ajax({
-			type:'POST',
-			url:'/verify_phone_number',
-			data:{
-				phone_number:$('#phone_number').val(),
-				csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
-			},
-			success:function(){
-				temp_phone = $('#phone_number').val();
+		type:'POST',
+		url:'/verify_phone_number',
+		data:{
+			phone_number:$('#phone_number').val(),
+			csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+		},
+		success:function(){
+			temp_phone = $('#phone_number').val();
 				//alert("Sent text sms " + temp_phone);
 			}
 		});
@@ -214,9 +226,9 @@ $(document).on('submit', '#VerifyPhoneNumber', function(e){
 $(document).on('submit', '#CustomerRequest', function(e){
 	e.preventDefault();
 	$.ajax({
-			type:'POST',
-			url:'/create_request',
-			data:{
+		type:'POST',
+		url:'/create_request',
+		data:{
 				pick_up_full_name:$('#pick_up_full_name').val(),	//This stuff will pass data from html-> views.py
 				pick_up_address:$('#pick_up_address').val(),
 				pick_up_city:$('#pick_up_city').val(),
@@ -245,7 +257,7 @@ $(document).on('submit', '#CustomerRequest', function(e){
 		});
 });		
 
-	
+
 
 
 //-----Zoom picture on amazon upload form-----
@@ -256,17 +268,17 @@ var img = document.getElementById("amazon_screenshot");
 var modalImg = document.getElementById("amazon_screenshot_modal");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
+	modal.style.display = "block";
+	modalImg.src = this.src;
 } 
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 
 }
 
