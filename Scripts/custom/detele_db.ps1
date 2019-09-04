1d85247e074db2e6e476a0eb4724dcfd878299d8
nvk1196 <48897412+nvk1196@users.noreply.github.com>
2019-09-04 18:35:02 -0500

    new scripts

---
+++ Scripts/custom/detele_db.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\Khanh\
+Remove-Item -path C:\Users\Khanh\khanh_project\flashshipper\src\Overview\migrations -exclude __init__.py –recurse
+Remove-Item –path C:\Users\Khanh\khanh_project\flashshipper -include *.pyc –recurse
+Remove-Item -path C:\Users\Khanh\khanh_project\flashshipper\src\db.fs_database
+cd C:\Users\Khanh\khanh_project\flashshipper\Scripts\custom
---
+++ Scripts/custom/run_server.ps1
@@ -0,0 +1,7 @@
+cd C:\Users\Khanh\khanh_project\flashshipper
+. .\Scripts\activate
+cd C:\Users\Khanh\khanh_project\flashshipper\src
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+cd C:\Users\Khanh\khanh_project\flashshipper\Scripts\custom
